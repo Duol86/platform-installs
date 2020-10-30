3ee96abfef094934e83c476714662825d81ac5d9
Duol86 <70291572+Duol86@users.noreply.github.com>
2020-10-30 21:22:07 +0000

    Update and rename catalinastuff.sh to macstuff.sh

--- macstuff.sh
+++ macstuff.sh
@@ -25,3 +25,19 @@ brew install wget
 wget https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.9/npp.7.9.Installer.exe
 
 wine npp.7.9.Installer.exe
+
+curl -O https://distfiles.macports.org/MacPorts/MacPorts-2.6.3.tar.bz2
+
+tar xf MacPorts-2.6.3.tar.bz2
+
+cd MacPorts-2.6.3/
+
+./configure
+
+make
+
+sudo make install
+
+cd
+
+sudo port install kdenlive
