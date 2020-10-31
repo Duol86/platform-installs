158492179657f40951a913c2e06dc8da5f7fbcd9
Duol86 <70291572+Duol86@users.noreply.github.com>
2020-10-31 00:47:32 +0000

    Create archstuff.py

---
+++ archstuff.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+name = "program-installs"
+
+print("Welcome to the " + name + " scripts!")
+
+import os
+os.system("sudo pacman -S vlc steam obs-studio gimp chromium git lutris wine-stable wget openvpn dialog python-pip python-setuptools flatpak gnome-software jre-openjdk")
+os.system("yay -S kdenlive")
+os.system("sudo pip3 install protonvpn-cli")
+os.system("flatpak install spotify")
+os.system("wget https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.9/npp.7.9.Installer.exe")
+os.system("wine npp.7.9.Installer.exe")
