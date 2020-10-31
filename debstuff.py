c4c01c739757b9cca8c8c033587cd4fa1fc2e0b4
Duol86 <70291572+Duol86@users.noreply.github.com>
2020-10-31 01:10:04 +0000

    Create debstuff.py

---
+++ debstuff.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import os
+os.system("sudo apt install openvpn dialog python3-pip python3-setuptools git spotify vlc steam minecraft gimp chromium ffmpeg obs-studio gnome-software kdenlive wget openjdk-8-jre
+os.system("sudo pip3 install protonvpn-cli")
+os.system("echo "deb http://download.opensuse.org/repositories/home:/strycore/Debian_10/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list")
+os.system("wget -q https://download.opensuse.org/repositories/home:/strycore/Debian_10/Release.key -O- | sudo apt-key add -")
+
+os.system("wget https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.9/npp.7.9.Installer.exe")
+os.system("wine npp.7.9.Installer.exe")
+
