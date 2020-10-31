044076057c721e137b53d7c002ba0f5ac358b065
Duol86 <70291572+Duol86@users.noreply.github.com>
2020-10-31 01:36:43 +0000

    Update fedorastuff.py

--- fedorastuff.py
+++ fedorastuff.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+print("Welcome to the platform-installs scripts")
+
 import os
 os.system("sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm")
 os.system("sudo dnf install xorg-xll-drv-nvidia-cuda spotify vlc steam lutris gimp chromium git openvpn dialog python3-pip python3-setuptools gnome-software kdenlive wine-stable wget")
