91a596eb301b5f9cdba1bbf11f39442916fa9cc7
Duol86 <70291572+Duol86@users.noreply.github.com>
2020-10-31 02:45:59 +0000

    Update debstuff.py

--- debstuff.py
+++ debstuff.py
@@ -3,7 +3,7 @@
 print("Welcome to the platform-installs scripts")
 
 import os
-os.system("sudo apt install openvpn dialog python3-pip python3-setuptools git spotify vlc steam minecraft gimp chromium ffmpeg obs-studio gnome-software kdenlive wget openjdk-8-jre
+os.system("sudo apt install discord openvpn dialog python3-pip python3-setuptools git spotify vlc steam minecraft gimp chromium ffmpeg obs-studio gnome-software kdenlive wget openjdk-8-jre
 os.system("sudo pip3 install protonvpn-cli")
 os.system("echo "deb http://download.opensuse.org/repositories/home:/strycore/Debian_10/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list")
 os.system("wget -q https://download.opensuse.org/repositories/home:/strycore/Debian_10/Release.key -O- | sudo apt-key add -")
